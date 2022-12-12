5b9858a15fc28662d78034d27aec3da153a1e20b
Olga Litvinenko <90822400+Risu0585@users.noreply.github.com>
2022-12-12 15:12:57 +0300

    Update header.php

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
                            <span class="icon-bar"></span>
                            <span class="icon-bar"></span>
                        </button>
-                       <a href="index.php" class="navbar-brand">магазин нейм</a>
+                       <a href="index.php" class="navbar-brand">CliviaStore</a>
                    </div>
                    
                    <div class="collapse navbar-collapse" id="myNavbar">
